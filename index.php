3f3d0b224d1c3fff9ea7a97f450eeb3761d7bf64
Lourenz Lactaoen <52141596+glitchHub-org@users.noreply.github.com>
2019-08-03 01:12:27 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+        <title>Home</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Reflections
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="reflection1.html">Refleciton 1</a>
+                  <a class="dropdown-item" href="reflection2.html">Refleciton 2</a>
+                  <a class="dropdown-item" href="reflection3.html">Refleciton 3</a>
+                  <a class="dropdown-item" href="reflection4.html">Refleciton 4</a>
+                </div>
+              </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Activities
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Activity 1</a>
+                <a class="dropdown-item" href="#">Activity 2</a>
+                <a class="dropdown-item" href="#">Activity 3</a>
+                <a class="dropdown-item" href="#">Activity 4</a>
+                <a class="dropdown-item" href="#">Activity 5</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  About
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">    
+                  <a class="dropdown-item" href="test.html">Contacts</a>
+                  <a class="dropdown-item" href="index.php">About Me</a>
+                </div>
+        </div>
+      </nav>
+<p>Welcome to this Website. My name is Lourenz, you can call me skip!</p>
+<p>From this website, you will know about the things I like or whatever I put here on a whim</p>
+<p><strong>Note:</strong> This is just a prototype website layout so this might be buggy.</p>
+
+</html>
